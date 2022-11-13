81a21da186f2465dba061b260e222e14e2764d73
aditya-majhi <majhi2723@gmail.com>
2022-11-13 23:40:24 +0530

    adjustments on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
           <div className='login flex flex-col justify-center items-center bg-white w-[300px] md:w-[400px] rounded-lg m-auto space-y-3 pb-4'>
            <img src={profile} alt='profile' className='w-[100px] mt-4 rounded-full'/>
             <h1 className='text-2xl pt-2 font-semibold md:text-3xl'>Log In to Account</h1>
-            <p className='text-sm'>Don't have an Account?<span className='text-blue-500 underline' onClick={() => {setLogin(false)}}>Sign in</span> Here.</p>
+            <p className='text-sm'>Don't have an Account?<span className='text-blue-500 underline cursor-pointer' onClick={() => {setLogin(false)}}>Sign in</span> Here.</p>
             <div className='space-y-2'>
               <div className='email bg-gray-100 px-2 py-1 space-x-2 rounded-md border'>
                 <i className="fa-solid fa-envelope text-gray-400"></i>
@@ -28,7 +28,7 @@ const Login = () => {
 
           <div className='signin flex flex-col justify-center items-center bg-white w-[300px] md:w-[400px] rounded-lg m-auto space-y-3 pb-4'>
             <h1 className='text-2xl pt-3 font-semibold md:text-3xl'>Create Account</h1>
-            <p className='text-sm'>Already have an Account?<span className='text-blue-500 underline' onClick={() => {setLogin(true)}}>Log in</span> Here.</p>
+            <p className='text-sm'>Already have an Account?<span className='text-blue-500 underline cursor-pointer' onClick={() => {setLogin(true)}}>Log in</span> Here.</p>
             <div className='space-y-2'>
               <div className='username bg-gray-100 px-2 py-1 space-x-2 rounded-md border'>
                 <i className="fa-solid fa-user text-gray-400"></i>
